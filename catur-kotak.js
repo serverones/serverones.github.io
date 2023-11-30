b07d602b845f073b280b399cd35630ce7c848cc8
serverones <serverones@hotmail.com>
2023-11-30 22:58:05 +0700

    upload HTML file

---
+++ catur-kotak.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '11c10afabb81ba52c0569a7643ad5c41',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highcpmcreativeformat.com/11c10afabb81ba52c0569a7643ad5c41/invoke.js"></scr' + 'ipt>');
---
+++ catur-small.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '8d6b45e256f1124ca7b7051ddc097466',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highcpmcreativeformat.com/8d6b45e256f1124ca7b7051ddc097466/invoke.js"></scr' + 'ipt>');
